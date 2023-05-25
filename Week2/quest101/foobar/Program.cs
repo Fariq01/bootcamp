ec34bb024ec923b1e77f9731b34e8a1e4de000ad
fariq <fariqfadhlan101@gmail.com>
2023-05-25 10:05:15 +0700

    skraaa

---
+++ Week2/quest101/foobar/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using dictiClass;
+
+
+int angka = Convert.ToInt32(Console.ReadLine());
+
+dictionaryClass dict1 = new dictionaryClass();
+
+string hasil = dict1.calculate(angka);
+
+Console.WriteLine(hasil);
---
+++ Week2/quest101/foobar/dictionaryClass.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+namespace dictiClass;
+
+    public class dictionaryClass
+    {
+
+        Dictionary<int, string> kamus = new Dictionary<int, string>
+        {
+            {3,  "foo"},
+            {5,  "bar"},
+            {7,  "soy"},
+        };
+
+        public string DictionaryMethod(int angka, Dictionary<int, string> kamus)
+        {
+
+            string hasil = "";
+
+            foreach (var kvp in kamus)
+            {
+                if(angka % kvp.Key == 0)
+                {
+                    hasil += kvp.Value;
+                }
+                else if(string.IsNullOrEmpty(hasil))
+                     {
+                        hasil = angka.ToString();
+                     }
+            }
+
+            return hasil;
+        }
+
+        public string calculate(int angka)
+        {
+            string hasil = "";
+            for(int i = 1; i <= angka; i++)
+            {
+                hasil += DictionaryMethod(i, kamus);
+            }
+
+            return hasil;
+        }
+
+
+    }
