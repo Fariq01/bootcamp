4a39051cac52d463465b5497b31e7b0f388bc2f2
fariq01 <fariqfadhlan101@gmail.com>
2023-06-09 17:01:13 +0700

    Concurrency&asynchronus

---
+++ Week4/4day5/Concurrency&asynchronus/Thread/DailyTask.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ThreadC
+{
+	public class DailyTask
+	{
+		
+		public void Study()
+		
+		{
+			Console.WriteLine("Studyng ...");
+		}
+		
+		public void Cleaning()
+		
+		{
+			Console.WriteLine("Cleaning ...");
+		}
+	}
+}
---
+++ Week4/4day5/Concurrency&asynchronus/Thread/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+using ThreadC;
+
+static class Program
+
+{
+	static void Main()
+	
+	{
+		//Pake thread
+		Stopwatch stopwatch = new();
+		DailyTask dt = new();
+		Thread thread1 = new(dt.Study);
+		Thread thread2 = new(dt.Cleaning);
+		thread1.Name = "Study Thread";
+		thread2.Name = "Cleaning Thread";
+		
+		stopwatch.Start();
+		
+		Console.WriteLine($"Thread {Thread.CurrentThread.Name} started.");
+		thread1.Start();
+		
+		//Gk dipake karena main thread di block, aktif tapi gk jalanin tasknya
+		//mending pake async.await
+		Thread.Sleep(5000);
+		thread2.Start();
+		
+		stopwatch.Stop();
+		
+		Console.WriteLine($"Thread {Thread.CurrentThread.Name} Finished.");
+		Console.WriteLine($"Program complete. Elapsed time: {stopwatch.ElapsedMilliseconds}ms");
+	}
+}
