3c17d4f013bf4c0a00ef7fad64f533c14f113387
fariq01 <fariqfadhlan101@gmail.com>
2023-06-13 16:54:51 +0700

    StreamsIO

---
+++ Week5/5day2/StreamsIO/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+static class Program
+{
+	static void Main()
+	{
+		
+		string filePath = "file2.txt";
+		
+		// StreamWriter file1 = new StreamWriter(File.Create(filePath));
+		
+		using(StreamWriter writer = new StreamWriter(File.Open(filePath, FileMode.Append)))
+		
+		{
+			writer.Write("Hello");
+			writer.Write(123131);
+		}
+
+		using(StreamReader reader = new StreamReader(File.Open(filePath, FileMode.Open)))
+		{
+			string input = reader.ReadLine();
+			Console.WriteLine(input);
+		}
+		
+		using (Strea)
+		{
+			
+		}
+	}
+}
